8014f4b6de4d142d53a1ebd94c9ed129a9cf08e3
Sanket <sanketnaik99@gmail.com>
2018-09-30 22:16:02 +0530

    Images Updated, Code Updated

--- Arduino/ArduinoBluetoothControl/ArduinoBluetoothControl.ino
+++ Arduino/ArduinoBluetoothControl/ArduinoBluetoothControl.ino
@@ -1,13 +1,18 @@
 #include <SoftwareSerial.h>
 SoftwareSerial BTserial(3, 4); // RX | TX
-int state = 0;
+int state1 = 0;
 int message;
 int Dev1 = 2;  //Defining the First Pin of the relay module
+// DEFINE YOUR NEW PINS HERE
+
+
 void setup()
 {
  pinMode(Dev1,OUTPUT);  //Setting up the First pin of Relay module as OUTPUT Pin
  digitalWrite(Dev1,HIGH);
  BTserial.begin(9600); //Setting up Bluetooth Software Serial
+
+//SETUP THE PINS BY COPYING THE CODE SNIPPETS AND ADDING THE DEVICE NAME
 }
  
  
@@ -19,14 +24,16 @@ void loop()
 
       //Flipping relay switch accoring to the current state
       if(message = 7){
-      if(state == 0){
-        state = 1;
+      if(state1 == 0){
+        state1 = 1;
         digitalWrite(Dev1,LOW);
-      }else if (state == 1) {
-        state = 0;
+      }else if (state1 == 1) {
+        state1 = 0;
         digitalWrite(Dev1, HIGH);
       }
-      //End of code for First Pin of the relay module  
+      //End of code for First Pin of the relay module 
+
+       //ADD CODE FOR NEW RELAY MODULE PINS HERE
     }
     }
   delay(500);
--- RaspberryPi/RaspberryPiBluetoothControl.py
+++ RaspberryPi/RaspberryPiBluetoothControl.py
@@ -2,14 +2,22 @@ import bluetooth
 import RPi.GPIO as GPIO  # calling for header file which helps in using GPIOs of PI
 
 
-Dev1 = 17  #The GPIO pin for the first relay module
+Dev1 = 17   #The GPIO pin for the first relay module
+            #Copy the same declaration again and give a pin number to each device
+
+status1 = 0 #Status Flag for the Device
+
+# DECLARE THE STATUS FLAG FOR A NEW DEVICE
+
 
 GPIO.setmode(GPIO.BCM)     #programming the GPIO by BCM pin numbers. (like PIN40 as GPIO21)
 GPIO.setwarnings(False)
-GPIO.setup(Dev1,GPIO.OUT)
+GPIO.setup(Dev1,GPIO.OUT) 
 GPIO.output(Dev1,1)
 
-status1 = 0 #Status Flag for the Device
+# ADD THE NEW DEVICES YOU WANT TO SETUP OVER HERE
+
+
 
 
 def flipStatus(stat):  #Function to flip the status Flag
@@ -42,6 +50,9 @@ while 1:
                 GPIO.output(Dev1,status1)
                 print ("DEV1 Switched")
                 status1 = flipStatus(status1)
+
+            # ADD A UNIQUE DATA VALUE IN THE IF STATEMENT AND WRITE THE NAME OF THE
+            # STATUS FLAG IN THE SNIPPET
                 
     
             if (data == 99):
